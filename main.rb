9b19e05784e2af099baa34f7413079ebda2d35d9
Diego Iturra <diturra@dparadig.com>
2025-10-21 01:17:54 -0300

    feat: create a method that calculate the total value of stock in the current portfolio, and add a new data structure to keep track of the allocation percentages

--- main.rb
+++ main.rb
@@ -10,6 +10,7 @@
 class Portfolio
 	def initialize()
 		@allocated_stocks = []
+		@target_allocation = {}
 	end
 
 	def rebalance()
@@ -19,10 +20,22 @@ class Portfolio
 		@allocated_stocks << { stock: stock, units: units }
 	end
 
+	def set_target_allocation(stock_name, percentage)
+		@target_allocation[stock_name] = percentage
+	end
+
 	def show_stocks()
 		@allocated_stocks.each do |stock|
 			puts "Stock name: #{stock[:stock].name}, Stock price #{stock[:stock].price}, Stock units: #{stock[:units]}"
 		end
+
+		puts "total_value: #{get_total_value}"
+	end
+
+	private
+	def get_total_value
+		total_value = @allocated_stocks.sum{ |allocation| allocation[:stock].price * allocation[:units]}
+		return total_value
 	end
 
 	def sold()
@@ -47,13 +60,20 @@ class Stock
 
 end
 
+# Se crean las stock con nombre y precio
 stock_1 = Stock.new("Nvidia", 1000)
 stock_2 = Stock.new("Apple", 2000)
 stock_3 = Stock.new("Meta", 3000)
 
+# Se crea el portafolio y se agregan las stocks al portafolio
 portfolio = Portfolio.new
 portfolio.add_stock(stock_1, 20)
 portfolio.add_stock(stock_2, 30)
 portfolio.add_stock(stock_3, 40)
+
+# Se setean los porcentajes objetivo
+portfolio.set_target_allocation("Nvidia", 0.1)
+portfolio.set_target_allocation("Apple", 0.5)
+portfolio.set_target_allocation("Meta", 0.9)
 
 portfolio.show_stocks()
