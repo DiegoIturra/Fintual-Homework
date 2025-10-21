345b592e15486714ed05bcea80a1c327b775f916
Diego Iturra <diturra@dparadig.com>
2025-10-21 00:55:03 -0300

    feat: add basic operation of create an stock with a price

--- main.rb
+++ main.rb
@@ -7,13 +7,50 @@
 # Add documentation/comments to understand your thinking process and solution
 # Important: If you use LLMs that’s ok, but you must share the conversations.
 
-
 class Portfolio
+	def initialize()
+		@allocated_stocks = []
+	end
+
+	def rebalance()
+	end
+
+	def add_stock(stock, units)
+		@allocated_stocks << { stock: stock, units: units }
+	end
+
+	def show_stocks()
+		@allocated_stocks.each do |stock|
+			puts stock[:stock].price, stock[:units]
+		end
+	end
+
+	def sold()
+	end
+
+	def buy()
+	end
 end
 
 class Stock
 
+	attr_reader :price
+
+	def initialize(price)
+		@price = price
+	end
+
 	def current_price(last_available_price)
+		@price = last_available_price
 	end
 
 end
+
+meta = Stock.new(100)
+apple = Stock.new(150)
+
+portfolio = Portfolio.new
+
+portfolio.add_stock(meta, 2)
+portfolio.add_stock(apple, 4)
+portfolio.show_stocks()
